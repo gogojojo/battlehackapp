d4220e0788b8513535a97efda9623492a6997e86
gogojojo <joseph.gochua@gmail.com>
2015-03-01 02:17:57 -0800

    user can pay and is reflected

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,17 +4,19 @@ class OrdersController < ApplicationController
   end
   def create
   config.logger = Logger.new(params[:number])
-  amount = params[:number]
+  @amount = params[:number]
   nonce = params[:payment_method_nonce]
   render action: :new and return unless nonce
   result = Braintree::Transaction.sale(
-    amount: amount, 
+    amount: @amount, 
     payment_method_nonce: nonce
   )
   redirect_to  event_path(params[:id])
   event = Event.find(params[:id])
   puts event.cost
-  event.cost = (event.cost.to_f - amount.to_f)
+  event.cost = (event.cost.to_f - @amount.to_f)
+  index = event.participants.index(current_user.id.to_s)
+  event.amount[index] = @amount 
   event.save
   end
 end
