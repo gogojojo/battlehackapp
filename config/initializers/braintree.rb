08a77fab11096fbafd95e66c1c1c2a4a21002c3c
althe1 <alkirakosian@gmail.com>
2015-03-02 09:39:46 -0800

    final changes

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -2,3 +2,4 @@ Braintree::Configuration.environment  =  :sandbox
 Braintree::Configuration.merchant_id   = ENV['BRAINTREE_MERCHANT_ID'] || 'your merchant id'
 Braintree::Configuration.public_key   = ENV['BRAINTREE_PUBLIC_KEY']  || 'your public key'
 Braintree::Configuration.private_key  = ENV['BRAINTREE_PRIVATE_KEY'] || 'your private key'
+
